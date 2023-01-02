09edcd214d8e357d46125067ad96710a6ef8e1bd
underrooz <ParmarTarun>
2023-01-02 17:16:40 -0500

    :sparkles: Add Tab Functionality

--- src/components/Auth Form/Main.js
+++ src/components/Auth Form/Main.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import "./UI/authform.css";
 import LoginForm from "./UI/LoginForm";
 import SignUpForm from "./UI/SignUpForm";
+import Tabs from "./UI/Tabs";
 
 const Main = () => {
-    const [loggingIn, setLogginIn] = useState(false);
+    const [loggingIn, setLogginIn] = useState(true);
 
     return (
         <div className="container">
             <div className="form-wrapper">
+                <Tabs toggleAuth={() => setLogginIn((ps) => !ps)} />
                 {loggingIn ? <LoginForm /> : <SignUpForm />}
             </div>
         </div>
---
+++ src/components/Auth Form/UI/Tabs.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Tabs = ({ toggleAuth }) => {
+    const handleTab = (e) => {
+        document
+            .querySelectorAll(".tabs button")
+            .forEach((el) => el.classList.remove("active"));
+        e.target.classList.add("active");
+        toggleAuth();
+    };
+
+    return (
+        <div className="tabs">
+            <button className="active" onClick={handleTab}>
+                LOG IN
+            </button>
+            <button onClick={handleTab}>SIGN UP</button>
+        </div>
+    );
+};
+
+export default Tabs;
