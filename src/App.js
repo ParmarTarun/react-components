33c738e28d1ad9278b262bb053327a8b897a88d5
underrooz <ParmarTarun>
2023-01-03 20:36:44 -0500

    :tada: Finalize

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
-import Main from "./components/Auth Form/Main";
 import Components from "./components/Components";
 
 const App = () => {
     return (
         <div className="app">
-            {/* <div className="app-header">
+            <div className="app-header">
                 <h1>React Components</h1>
-            </div> */}
-            {/* <Components /> */}
-            <Main />
+            </div>
+            <Components />
         </div>
     );
 };
